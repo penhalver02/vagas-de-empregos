57eec89174d596e69abfce462fb48ee2a66eeb18
Lucas Penhalver <penhalver.lucas@gmail.com>
2020-05-13 20:50:40 -0300

    visitante entra na home page

---
+++ spec/features/visitor/visitor_open _home_page_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature 'Vistor open home page' do
+  scenario 'successfully' do
+    visit root_path
+
+    expect(page).to have_link('Registrar-se')
+    expect(page).to have_link('Registrar recrutador')
+    expect(page).to have_link('Vagas')
+  end
+end
